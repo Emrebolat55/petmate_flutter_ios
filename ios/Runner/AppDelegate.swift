90d678aca702d49418996f56732ca0923f8a47fe
Emrebolat55 <bolatemre574@gmail.com>
2026-03-13 01:46:04 +0300

    Fix syntax error in AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,13 @@
 import Flutter
 import UIKit
 import GoogleMaps
-GMSServices.provideAPIKey("AIzaSyDSFRCqlocCz_L-UAai2S91JiriKb4xyGY")
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDSFRCqlocCz_L-UAai2S91JiriKb4xyGY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
